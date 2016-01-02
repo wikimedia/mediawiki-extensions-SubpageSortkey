e581cb8fea32f3c2dd0b6c4e6569cb708c39f1c9
PranavK <pranavmk98@gmail.com>
2016-01-02 07:02:36 +0530

    Convert SubpageSortkey to use extension registration
    Bug: T87975
    Change-Id: I4762af9539ab5362ccf3037e4367df1fe1650f05

--- SubpageSortkey.php
+++ SubpageSortkey.php
@@ -1,62 +1,23 @@
 <?php
 /**
- * Really simple extension to change the default sortkey to have something
- * to do with the subpages. See bug 22911. Requires at least MediaWiki 1.18
- *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by the Free
- *  Software Foundation; either version 2 of the License, or (at your option)
- *  any later version.
- *
- *  This program is distributed in the hope that it will be useful, but WITHOUT
- *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- *  more details.
- *
- *  You should have received a copy of the GNU General Public License along with
- *  this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- *  Place - Suite 330, Boston, MA 02111-1307, USA.
- *  http://www.gnu.org/copyleft/gpl.html
- *
  * @addtogroup Extensions
  *
  * @author Brian Wolff
  * @copyright Copyright © 2011 Brian Wolff
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- */
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'Subpage Sortkey',
-	'author' => array( '[http://mediawiki.org/wiki/User:Bawolff Brian Wolff]' ),
-	'descriptionmsg' => 'subpagesortkey-desc',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:SubpageSortkey',
-	'version' => '0.2.0',
-	'license-name' => 'GPL-2.0+',
-);
+*/
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'SubpageSortkey' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['SubpageSortkey'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['SubpageSortkeyAlias'] = __DIR__ . '/SubpageSortkey.alias.php';
+	/* wfWarn(
+		'Deprecated PHP entry point used for SubpageSortkey extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
 
-// Syntax is as follows:
-// * A positive number n represents the nth subpage section
-// * A negative number -i represents the ith subpage section counting backwards
-// * A range a..b represents all sections a to b inclusive
-// * Ranges can be open.
-// Examples:
-// "0" would be just the base page name (a/b/c/d -> a).
-// "0..2" would be the first 3 sections (a/b/c/d -> a/b/c)
-// "0,2" would be a/b/c/d -> a/c
-// "1.." would be a/b/c/d -> b/c/d
-// "0,-2" would be a/b/c/d -> a/c
-
-// The default sortkey subpage descriptor (aka "1,2,3..6" )
-$wgSubpageSortkeyDefault = false;
-// Per namespace sortkeys (array, each key is a namespace number)
-// For example $wgSubpageSortkeyByNamespace[NS_TALK] = '1..';
-$wgSubpageSortkeyByNamespace = array();
-// If the subpage descriptor is empty, use the normal page name?
-$wgSubpageSortkeyIfNoSubpageUseFullName = true;
-
-$wgHooks['GetDefaultSortkey'][] = 'SubpageSortkey::onGetDefaultSortkey';
-
-$wgAutoloadClasses['SubpageSortkey'] = __DIR__ . '/SubpageSortkey_body.php';
-$wgMessagesDirs['SubpageSortkey'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['SubpageSortkey'] = __DIR__ . '/SubpageSortkey.i18n.php';
+	return;
+} else {
+	die( 'This version of the SubpageSortkey extension requires MediaWiki 1.25+' );
+}
 
