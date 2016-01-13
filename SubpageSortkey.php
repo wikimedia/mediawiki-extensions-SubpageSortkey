7329c3186567f58c1bc9458f9d032b6d1e3eb8a7
Reedy <reedy@wikimedia.org>
2016-01-13 00:38:46 +0000

    No alias file in extension
    Change-Id: I9f3f1f60ca91c676aed320ea1ad7791fae503e1a

--- SubpageSortkey.php
+++ SubpageSortkey.php
@@ -10,7 +10,6 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'SubpageSortkey' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['SubpageSortkey'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['SubpageSortkeyAlias'] = __DIR__ . '/SubpageSortkey.alias.php';
 	/* wfWarn(
 		'Deprecated PHP entry point used for SubpageSortkey extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
