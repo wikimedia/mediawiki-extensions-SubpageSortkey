72219ea996d7d7ad6120e3db9874e628f79ee029
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 21:58:05 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: I2319cc70bb2f72cff3ab5c1c64f8d6e88218e527

--- SubpageSortkey.php
+++ SubpageSortkey.php
@@ -10,11 +10,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'SubpageSortkey' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['SubpageSortkey'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for SubpageSortkey extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 
 	return;
 } else {
